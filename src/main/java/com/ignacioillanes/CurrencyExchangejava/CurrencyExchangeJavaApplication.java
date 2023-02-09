ba2e12a5a1b51cb42622ca95f329ddb8ef488110
iguiIllanes <igui10082002@gmail.com>
2023-02-08 23:13:36 -0400

    Initial commit

---
+++ src/main/java/com/ignacioillanes/CurrencyExchangejava/CurrencyExchangeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ignacioillanes.CurrencyExchangejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeJavaApplication.class, args);
+	}
+
+}
